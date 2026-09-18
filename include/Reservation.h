d3b64a641f1014e34e6c5bc1301865a7fca6a5d8
Michaela1414 <michaelabosmans@my.unt.edu>
2026-09-18 01:25:11 -0500

    Update Reservation.h

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,20 @@
+class Reservation
+{
+private:
+    int reservationID;
+    int studentID;
+    string studentName;
+    string resourceID;
+    string reservationDate;
 
+public:
+    // constructor
+    Reservation(...);
+
+    // getters
+    int getReservationID();
+    int getStudentID();
+    string getStudentName();
+    string getResourceID();
+    string getReservationDate();
+};
