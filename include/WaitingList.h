69426c247d875a8ebd2add60355948e4f37bd201
Ruth Kalonji Omoyi <ruthomoyi@my.unt.edu>
2026-09-19 22:58:08 -0500

    Add WaitingList class with enqueue and dequeue methods
    The queue needs 2 pointers the front for who gets the resource and the rear where the next student would be added. The waiting list is for what resource the one who is waiting is waiting for. We can't void the dequeue because the manager needs the info, so the bool let us know when someone is removed or when the queue is empty. We void enqueue because students' information are not changing there. We use std::string for a clearer header.

--- include/WaitingList.h
+++ include/WaitingList.h
@@ -1,1 +1,34 @@
+#ifndef WAITINGLIST_H
+#define WAITINGLIST_H
 
+#include <string>
+
+class WaitingList
+{
+  private:
+      struct Node
+      {
+          int studentID;
+          std::string studentName;
+          std::string resourceID;
+          Node* next;
+      };
+
+      Node* front;
+      Node* rear;
+
+  public:
+    WaitingList();
+
+    void enqueue(int studentID,
+                 const std::string studentName,
+                 const std::string resourceID);
+    bool dequeue(int& studentID,
+                 std::string& studentName,
+                 std::string& resourceID);
+
+    bool isEmpty() const;
+
+    void display() const;
+};
+#endif
