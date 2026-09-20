7ead7b6dc269446eac3c8b4c2f0fa96679b67ebc
Ruth Kalonji Omoyi <ruthomoyi@my.unt.edu>
2026-09-20 13:59:18 -0500

    WaitingList implementation
    We start with an empty queue. We create new node to enqueue students' information into it. We dequeue from the front (FIFO) by checking if it is empty before saving the first node, then moving students' info to the front and delete the old node. We start displaying from the front then through the list.

--- src/WaitingList.cpp
+++ src/WaitingList.cpp
@@ -1,1 +1,79 @@
+#include ".../include/WaitingList.h"
 
+#include <iostream>
+using namespace std;
+
+WaitingList::WaitingList();
+{
+  front = nullptr;
+  rear = nullptr;
+}
+
+void WaitingList::enqueue(int studentID, const string& studentName, const string& resourceID);
+{
+  Node* newNode = new Node
+
+  newNode->studentID = studentID;
+  newNode->studentName = studentaname;
+  newNode->resourceID = resourceID;
+  newNode->next = nullptr;
+
+  if (rear == nullptr)
+  {
+    front = newNode;
+    rear = newNode;
+  }
+  else
+  {
+    rear->next = newNode;
+    rear = newNode;
+  }
+}
+
+bool WaitingList::dequeue(int& studentID, string& studentName, string& resourceID);
+{
+  if (fornt == nullptr)
+  {
+    return false;
+  }
+
+  Node* temp = front;
+
+  studentID = temp->studentID;
+  studentName = temp->studentName;
+  resourceID = temp->resourceID;
+
+  front = front->next;
+
+  if (front == nullptr)
+  {
+      rear = nullptr;
+  }
+  delete temp;
+  return true;
+}
+
+bool Waitinglist::isEmpty() const
+{
+  return front == nullptr;
+}
+
+void Waitinglist::display() const
+{
+  if (front == nullptr)
+  {
+      cout << "Waisting list is empty." << endl;
+      return;
+  }
+  Node* current = front;
+
+  while (current != nullptr)
+  {
+      cout << "Student ID: " << current->studentID << endl;
+      cout << "Student Name: " << current->studentName << endl;
+      cout << "Resource ID: " << current->resourceID << endl;
+      cout << "-------------------------" << endl;
+
+      current = current->next;
+  }
+}
